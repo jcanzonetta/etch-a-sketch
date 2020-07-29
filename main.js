c2c1953dfa90fac4715296cd66e4734fd6b00a0e
jcanz <3466642+jcanzonetta@users.noreply.github.com>
2020-07-28 20:03:26 -0700

    Add reset button with working resized grid-container

--- main.js
+++ main.js
@@ -8,19 +8,41 @@ squares.forEach(colorSquare);
 
 const resetButton = document.querySelector('#reset');
 
-resetButton.addEventListener("click", () => {
+resetButton.addEventListener("click", resetGrid)
 
-})
+function resetGrid(){
+    let rows;
+    let columns;    
 
+    do{
+        rows = parseInt(prompt("","rows"), 10);
+    } while (isNaN(rows) || rows < 0 || rows > 10000);
+    do{
+        columns = parseInt(prompt("","columns"), 10);
+    } while (isNaN(columns) || columns < 0 || columns > 10000);
 
-function setGridSize(rows, columns){
- 
+    squaresContainer.innerHTML = "";
+    setGridSize(rows,columns);
+    makeSquares(rows,columns);
+
+    const squares = document.querySelectorAll('.squares');
+    // squares.forEach(setSquareSizes(rows, columns), e);
+    squares.forEach(colorSquare);
 }
 
+// function setSquareSizes(rows, columns){
+//     console.log(e);
+//     e.style.height = "rows";
+//     e.style.width = "columns";
+// }
 
+function setGridSize(rows, columns){
+    let rowSizes = 100/(rows+0.5);
+    let columnSizes = 100/(columns+0.5);
 
-
-
+    squaresContainer.style.gridTemplateRows = `repeat(${rows}, ${rowSizes}vmin)`;
+    squaresContainer.style.gridTemplateColumns = `repeat(${columns}, ${columnSizes}vmin)`;
+}
 
 
 
